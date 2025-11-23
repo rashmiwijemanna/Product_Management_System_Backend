a27343c2de4218a77faa83c8863b67dad0333b40
Rashmi Wijemanna <rashmiwijemanna7@gmail.com>
2025-11-23 10:25:32 +0530

    add product and get all products implemented in controller class

--- src/main/java/edu/icet/controller/ProductController.java
+++ src/main/java/edu/icet/controller/ProductController.java
@@ -1,11 +1,27 @@
 package edu.icet.controller;
 
+import edu.icet.model.dto.ProductDTO;
+import edu.icet.service.ProductService;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
 
 @RestController
 @RequestMapping("product")
 public class ProductController {
+
+    @Autowired
+    ProductService productService;
+
+    @PostMapping("/add")
+    public void add(@RequestBody ProductDTO productDTO){
+        productService.add(productDTO);
+    }
+
+    @GetMapping("/getAll")
+    public List<ProductDTO> getAll(){
+       return productService.getAllProductsDetails();
+    }
 
 }
