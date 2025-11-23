baeb9009a34ea2af0039c58cab2ddcc4aa1d9ec9
Rashmi Wijemanna <rashmiwijemanna7@gmail.com>
2025-11-23 09:50:59 +0530

    add feilds in product dto class

--- src/main/java/edu/icet/model/dto/ProductDTO.java
+++ src/main/java/edu/icet/model/dto/ProductDTO.java
@@ -1,4 +1,17 @@
 package edu.icet.model.dto;
 
+import lombok.*;
+
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
+@ToString
+
 public class ProductDTO {
+    private String id;
+    private String name;
+    private String description;
+    private double unitPrice;
+    private int qtyOnHand;
 }
