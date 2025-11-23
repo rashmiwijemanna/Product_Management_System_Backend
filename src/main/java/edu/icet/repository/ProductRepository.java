7efac3239c5d699cbf6a7e20f332eef1fb6c5284
Rashmi Wijemanna <rashmiwijemanna7@gmail.com>
2025-11-23 10:03:20 +0530

    aextends JpaRepository to ProductRepository

--- src/main/java/edu/icet/repository/ProductRepository.java
+++ src/main/java/edu/icet/repository/ProductRepository.java
@@ -1,4 +1,7 @@
 package edu.icet.repository;
 
-public interface ProductRepository {
+import edu.icet.model.entity.Product;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface ProductRepository extends JpaRepository<Product,String> {
 }
