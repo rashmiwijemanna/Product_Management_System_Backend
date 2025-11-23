b1dd70dc4bfebfec7b4e5e27723b9d96120b61c0
Rashmi Wijemanna <rashmiwijemanna7@gmail.com>
2025-11-23 10:14:36 +0530

    get all product details implemeted in service class

--- src/main/java/edu/icet/service/ProductService.java
+++ src/main/java/edu/icet/service/ProductService.java
@@ -1,7 +1,30 @@
 package edu.icet.service;
 
+import edu.icet.model.dto.ProductDTO;
+import edu.icet.model.entity.Product;
+import edu.icet.repository.ProductRepository;
+import org.modelmapper.ModelMapper;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+
+import java.util.ArrayList;
+import java.util.List;
 
 @Service
 public class ProductService {
+
+    @Autowired
+    ProductRepository productRepository;
+    private ModelMapper modelMapper=new ModelMapper();
+
+    public List<ProductDTO> getAllProductsDetails(){
+        List<Product> all = productRepository.findAll();
+        List<ProductDTO>productDTOS=new ArrayList<>();
+
+        for(Product product : all){
+            productDTOS.add(modelMapper.map(product,ProductDTO.class));
+
+        }
+        return productDTOS;
+    }
 }
