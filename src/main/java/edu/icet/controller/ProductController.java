db0365fa6147335f1b261d10ad2fd1a4f25b4415
Rashmi Wijemanna <rashmiwijemanna7@gmail.com>
2025-11-23 10:05:19 +0530

    put rest controller and request mapping annotaions

--- src/main/java/edu/icet/controller/ProductController.java
+++ src/main/java/edu/icet/controller/ProductController.java
@@ -1,4 +1,9 @@
 package edu.icet.controller;
 
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("product")
 public class ProductController {
 }
--- src/main/java/edu/icet/repository/ProductRepository.java
+++ src/main/java/edu/icet/repository/ProductRepository.java
@@ -2,6 +2,8 @@ package edu.icet.repository;
 
 import edu.icet.model.entity.Product;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
+@Repository
 public interface ProductRepository extends JpaRepository<Product,String> {
 }
