70e51d74225605b5f64fdebe3a71d93866604359
Rashmi Wijemanna <rashmiwijemanna7@gmail.com>
2025-11-23 09:53:51 +0530

    add feilds in dto class & implemet @id and @Entity annotations

--- src/main/java/edu/icet/model/entity/Product.java
+++ src/main/java/edu/icet/model/entity/Product.java
@@ -1,4 +1,20 @@
 package edu.icet.model.entity;
 
+import jakarta.persistence.Entity;
+import jakarta.persistence.Id;
+import lombok.*;
+
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
+@ToString
+@Entity
 public class Product {
+    @Id
+    private String id;
+    private String name;
+    private String description;
+    private double unitPrice;
+    private int qtyOnHand;
 }
